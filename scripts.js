752fc262270d4f6025206de5a979e9c7b7a7c8fa
Duong Huu Tuong <duonghuutuong0712@gmail.com>
2024-10-20 20:48:53 +0700

    Process dropdown menu

--- scripts.js
+++ scripts.js
@@ -47,3 +47,39 @@ $(document).ready(function() {
         synchronizeNavigation('#nav-footer', '#nav-header', this);
     });
 });
+
+
+const dropdown = document.querySelector('.dropdown');
+const select = dropdown.querySelector('.select');
+const caret = dropdown.querySelector('.caret');
+const menu = dropdown.querySelector('.menu');
+const options = dropdown.querySelectorAll('.menu li');
+const selected = dropdown.querySelector('.selected');
+
+// Toggle the dropdown menu when clicking on the select element
+select.addEventListener('click', () => {
+    select.classList.toggle('select-clicked');
+    caret.classList.toggle('caret-rotate');
+    menu.classList.toggle('menu-open');
+});
+
+// Loop through all options and add a click event
+options.forEach(option => {
+    option.addEventListener('click', () => {
+        // Change the selected text
+        selected.innerText = option.innerText;
+        // Close the dropdown menu
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        menu.classList.remove('menu-open');
+    });
+});
+
+// Close the dropdown menu if clicking outside
+document.addEventListener('click', (e) => {
+    if (!dropdown.contains(e.target)) {
+        select.classList.remove('select-clicked');
+        caret.classList.remove('caret-rotate');
+        menu.classList.remove('menu-open');
+    }
+});
